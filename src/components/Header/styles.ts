fc8fa52232dd95ef327572230d034aeda653b2eb
krasny <lookjota@gmail.com>
2023-08-19 19:33:16 -0300

    Criando component Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.Image`
+  width: 24px;
+  height: 55px;
+`
